fde2f9b78190e1e645cf91dcaa23e8f1b983f415
IgorMeloDev <igorjosimat@gmail.com>
2024-09-12 11:29:37 -0300

    up
    .

--- C/output/atividade 1/10.c
+++ C/output/atividade 1/10.c
@@ -17,3 +17,21 @@ int main() {
     }
 
     // Calcula o tamanho da palavra
+    tamanho = strlen(palavra);
+
+    // Inverte a palavra
+    for (i = tamanho - 1, j = 0; i >= 0; i--, j++) {
+        invertida[j] = palavra[i];
+    }
+    invertida[j] = '\0'; // Adiciona o caractere nulo no final da string invertida
+
+    // Verifica se a palavra original é igual à invertida
+    if (strcmp(palavra, invertida) == 0) {
+        printf("A palavra %s eh um palindromo.\n", palavra);
+    } else {
+        printf("A palavra %s nao eh um palindromo.\n", palavra);
+    }
+
+    return 0;
+}
+
