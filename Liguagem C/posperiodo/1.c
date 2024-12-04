9e6abc10b611f32fa4e5702d6dd784ce5c6355fe
IgorMeloDev <igorjosimat@gmail.com>
2024-12-04 14:16:12 -0300

    testando o Ia-Codeium

---
+++ Liguagem C/posperiodo/1.c
@@ -0,0 +1,7 @@
+//crie um programa que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+#include <stdio.h>
+int main(void) {
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+}
